182c8a3ca71f98ed5fa0edc02feadfbc5d079d11
Aqua Snake <87765985+Aqua-Snake@users.noreply.github.com>
2021-11-05 10:18:12 -0700

    New Log message〽

--- bot.js
+++ bot.js
@@ -400,7 +400,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please Wait.')}`);
                 if (config.FULLEVA == 'true') {
                     await conn.sendMessage(conn.user.jid, EVA_ACTİON, MessageType.text)
                 } else {
-                    await conn.sendMessage(conn.user.jid, '*CyberBot Working as Public! 🔗*\n\n_Please do not try plugins here. This is your LOG number._\n_You can try commands to any chat :)_\n\n*Your bot working as public. To change it, use* _.setvar WORK_TYPE:private_\n\n*Thanks for using CyberBot 💌*', MessageType.text);
+                    await conn.sendMessage(conn.user.jid, '*CyberBot Working as Public! 🔗*\n\n *Please do not try plugins here. This is your LOG number.*\n _You can try commands to any chat :)_\n\n *Your bot working as public. To change it, use* _.setvar WORK_TYPE:private_\n\n*Thanks for using CyberBot ✨* \n\n Here is our Website https://tinyurl.com/yjacqh8e you contact us.', MessageType.text);
                 }               
                 await git.fetch();
                 var commits = await git.log([config.BRANCH + '..origin/' + config.BRANCH]);
@@ -455,7 +455,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please Wait.')}`);
                 if (config.FULLEVA == 'true') {
                     await conn.sendMessage(conn.user.jid, EVA_ACTİON, MessageType.text)
                 } else {
-                    await conn.sendMessage(conn.user.jid, '*CyberBot Working as Private! 🔗*\n\n_Please do not try plugins here. This is your LOG number._\n_You can try commands to any chat :)_\n\n*Your bot working as private. To change it, use* _.setvar WORK_TYPE:public_\n\n*Thanks for using CyberBot 💌*', MessageType.text);
+                    await conn.sendMessage(conn.user.jid, '*CyberBot Working as Private! 🔗*\n\n *Please do not try plugins here. This is your LOG number.*\n _You can try commands to any chat :)_\n\n *Your bot working as private. To change it, use* _.setvar WORK_TYPE:public_\n\n*Thanks for using CyberBot ✨* \n\n Here is our Website https://tinyurl.com/yjacqh8e you contact us.', MessageType.text);
                 }
                 await git.fetch();
                 var commits = await git.log([config.BRANCH + '..origin/' + config.BRANCH]);
