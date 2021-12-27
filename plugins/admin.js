3ff6bdbf4a93979bad50bb9ddf32c1b36e49e179
Aqua Snake <87765985+Aqua-Snake@users.noreply.github.com>
2021-12-28 00:27:42 +0530

    Added Group link reset and Group name chnage

--- plugins/admin.js
+++ plugins/admin.js
@@ -6,14 +6,6 @@ you may not use this file except in compliance with the License.
 Cyber Bot - Aqua Snake
 */
 
-/* Copyright (C) 2020 Yusuf Usta.
-
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-
-WhatsAsena - Yusuf Usta
-*/
-
 const {MessageType, GroupSettingChange} = require('@adiwajshing/baileys');
 const Asena = require('../events');
 const Config = require('../config');
